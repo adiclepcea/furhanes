6244505c93adb901dc8dd0c18029f5b350274d76
Adrian Clepcea <adi_clepcea@yahoo.com>
2017-06-11 20:24:17 +0300

    Fix download lost on contract save

--- src/main/java/org/clepcea/dao/ContractDaoImpl.java
+++ src/main/java/org/clepcea/dao/ContractDaoImpl.java
@@ -25,6 +25,8 @@ public class ContractDaoImpl implements ContractDao {
 		Transaction tx = session.beginTransaction();
 		Contract c=this.getById(contract.getId());
 		contract.setSupplier(c.getSupplier());
+		contract.setOriginalFileName(c.getOriginalFileName());
+		contract.setScanFile(c.getScanFile());
 		session.saveOrUpdate(contract);
 		tx.commit();
 		session.close();
