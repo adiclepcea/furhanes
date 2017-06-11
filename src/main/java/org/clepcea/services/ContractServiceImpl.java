2552817d0dbb77409bc957c6eda2325161d563ec
Adrian Clepcea <adi_clepcea@yahoo.com>
2017-06-11 20:39:37 +0300

    Add date format to excel date cells

--- src/main/java/org/clepcea/services/ContractServiceImpl.java
+++ src/main/java/org/clepcea/services/ContractServiceImpl.java
@@ -8,6 +8,9 @@ import java.util.Map;
 
 import org.apache.poi.hssf.usermodel.HSSFWorkbook;
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellStyle;
+import org.apache.poi.ss.usermodel.CreationHelper;
+import org.apache.poi.ss.usermodel.Font;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
 import org.apache.poi.ss.usermodel.Workbook;
@@ -57,9 +60,14 @@ public class ContractServiceImpl implements ContractService {
 	public void writeListToExcel(int start, int count, Map<String, Object> filter, OutputStream os) throws IOException{
 		List<Contract> listContracts= listContracts(start, count, filter);
 		Workbook wkbook = new HSSFWorkbook();
+		CreationHelper createHelper = wkbook.getCreationHelper();
 		Sheet sh = wkbook.createSheet("Contracts");
 		int rowPos = 0;
 		Row r = sh.createRow(rowPos++);
+		Font font = wkbook.createFont();
+		font.setBold(true);
+		CellStyle headerStyle = wkbook.createCellStyle();
+		headerStyle.setFont(font);
 		r.createCell(0).setCellValue("Supplier");
 		r.createCell(1).setCellValue("Contract date");
 		r.createCell(2).setCellValue("Internal number");
@@ -69,12 +77,18 @@ public class ContractServiceImpl implements ContractService {
 		r.createCell(6).setCellValue("Do not renew");
 		r.createCell(7).setCellValue("Filed");
 		r.createCell(8).setCellValue("Payment Term");
+		for(int i=0;i<=8;i++){
+			r.getCell(i).setCellStyle(headerStyle);
+		}
+		CellStyle csDate=wkbook.createCellStyle();
+		csDate.setDataFormat(createHelper.createDataFormat().getFormat("dd.mm.yy"));
 		for(Contract contract:listContracts){
 			Row row = sh.createRow(rowPos++);
 			Cell cell = row.createCell(0);
 			cell.setCellValue(contract.getSupplier().getName());
 			Cell cell1 = row.createCell(1);
 			cell1.setCellValue(contract.getContractDate());
+			cell1.setCellStyle(csDate);
 			Cell cell2 = row.createCell(2);
 			cell2.setCellValue(contract.getInternalNumber());
 			Cell cell3 = row.createCell(3);
@@ -82,6 +96,7 @@ public class ContractServiceImpl implements ContractService {
 				cell3.setCellValue("");
 			}else{
 				cell3.setCellValue(contract.getExpirationDate());
+				cell3.setCellStyle(csDate);
 			}
 			
 			Cell cell4 = row.createCell(4);
